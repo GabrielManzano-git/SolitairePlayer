58e16b181b684b3ae17f031bae75e07d19284be9
unknown <gxm0243@mavs.uta.edu>
2025-08-27 10:25:07 -0500

    Named 1's as Aces

--- src/Card.java
+++ src/Card.java
@@ -24,7 +24,9 @@ public class Card{
 
     public void PrintCardRank(){
         switch(rank){
-            case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ->
+            case 1 ->
+                System.out.printf("%5s", "Ace");
+            case 2, 3, 4, 5, 6, 7, 8, 9, 10 ->
                 System.out.printf("%5d", rank);
             case 11 ->
                 System.out.printf("%5s", "Jack");
